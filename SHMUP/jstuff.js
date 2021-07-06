905a06ef70bcd41cf95d11d853db78915da9c13e
ChrisBSU <45082972+ChrisBSU@users.noreply.github.com>
2021-07-06 11:16:57 -0600

    Updated Ajax form
    update to var form #ajax

--- SHMUP/jstuff.js
+++ SHMUP/jstuff.js
@@ -7,7 +7,8 @@
     var formMessages = $('#form-messages');
 
     // Set up an event listener for the contact form.
-    $(form).submit(function (e) {
+	$(form).submit(function (e) {
+
         // Stop the browser from submitting the form.
         e.preventDefault();
 
